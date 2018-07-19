3e64b5fc1335314f277bd80701506000190b9fe4
david <david.journo.31@gmail.com>
2018-07-19 16:55:34 +0200

    lvl folder

---
+++ sandbox/public/levels/salon.js
@@ -0,0 +1,32 @@
+var salonState = {
+
+    // Create the Level 
+    create: function(){
+
+        // Background
+        game.add.sprite(0, 0, "sky");
+
+        // House
+
+        // Salon sprites
+        game.add.sprite(650,450,"buffet");
+        game.add.sprite(650,418,"lamp");
+        game.add.sprite(100,450,"chaise");
+        game.add.sprite(270,450,"chaise").scale.x*=-1;
+        game.add.sprite(200,380,"etagere3");
+        game.add.sprite(420,380,"fenetre");
+        game.add.sprite(400 - 64, 450 - 64,"porte");
+        game.add.sprite(150,450,"tv");
+    },
+
+    // If the player successfully finds all objects -  Then
+
+    /* Launch the Arrow function which make an arrow apear on screen, in order to navigate to the next
+        level, use 'E' on arrow to load it. */
+ 
+    // Else highlight elements that the player haven't found and move to the next level.
+    update: function(){
+        game.state.start('sdb');
+    }
+
+}
---
+++ sandbox/public/levels/sdb.js
@@ -0,0 +1,26 @@
+var sdbState = {
+
+    create: function () {
+
+        // Background
+        game.add.sprite(0, 0, "sky");
+
+        // House
+
+        // Salle de Bain
+        game.add.sprite(50, 300, "etagere1");
+        game.add.sprite(650, 300, "etagere2");
+        game.add.sprite(100, 400, "meuble");
+        game.add.sprite(200, 400, "douche");
+        game.add.sprite(300, 400, "porteserviette");
+        game.add.sprite(400 - 64, 400, "double_lavabo");
+        game.add.sprite(500, 350, "peignoire");
+        game.add.sprite(600, 450, "toilettes");
+        game.add.sprite(700, 450, "pq");
+
+    },
+    update: function(){
+        game.state.start('chambre');
+    }
+
+}
