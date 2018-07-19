1936fdb2219f9736b0de6e802ba14ac3a006f0a8
david <david.journo.31@gmail.com>
2018-07-19 13:18:22 +0200

    keymap

--- sandbox/public/js/menu.js
+++ sandbox/public/js/menu.js
@@ -2,11 +2,11 @@ var menuState = {
     create: function(){
         var nameLabel = game.add.text(80, 80, 'My first game',{ font: '50px Arial', fill: '#ffffff'});
 
-        var startLabel = game.add.text(80, game.world.height-80, 'press the "enter" key to start',{ font: '25px Arial', fill: '#ffffff'});
+        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to start',{ font: '25px Arial', fill: '#ffffff'});
 
-        var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.Enter);
+        var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
-        enterKey.onDown.addOnce(this.start, this);
+        wKey.onDown.addOnce(this.start, this);
     },
     start: function(){
         game.state.start('play');
