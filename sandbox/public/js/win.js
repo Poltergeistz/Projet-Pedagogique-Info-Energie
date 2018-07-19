ffb6df89c2403a2abbd5963a164a2ad242c6abcd
david <david.journo.31@gmail.com>
2018-07-19 13:18:53 +0200

    keymap

--- sandbox/public/js/win.js
+++ sandbox/public/js/win.js
@@ -3,11 +3,11 @@ var winState = {
 
         var winLabel = game.add.text(80,80, 'YOU WON!', { font: '50px Arial', fill: '#00FF00'});
 
-        var startLabel = game.add.text(80, game.world.height-80, 'press the "enter" key to restart', { font: '25px Arial', fill: '#ffffff'})
+        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart', { font: '25px Arial', fill: '#ffffff'})
    
-        var enterKey = game.input.keyboard.addKey(Phaser.Keyboard.Enter);
+        var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
 
-        enterKey.onDown.addOnce(this.start, this);
+        wKey.onDown.addOnce(this.start, this);
     },
     restart: function(){
         game.state.start('menu');
