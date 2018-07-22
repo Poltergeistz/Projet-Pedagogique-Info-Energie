f080eb3aa2ee6f1acde87cb1157aeae1bd48731c
david <david.journo.31@gmail.com>
2018-07-22 11:43:54 +0200

    translated

--- sandbox/public/js/intro.js
+++ sandbox/public/js/intro.js
@@ -1,7 +1,7 @@
 var introState = {
 
 	preload: function () {
-        var nameLabel = game.add.text(80, 80, 'THIS IS WHERE \n THE STORY BEGIN',{ font: '50px Arial', fill: '#ffffff', align: 'center'});
+        var nameLabel = game.add.text(80, 80, 'VOTRE HISTOIRE COMMENCE \n ICI!',{ font: '50px Arial', fill: '#ffffff', align: 'center'});
 
         // player = game.add.sprite(16,16, "");
         // game.physics.enable(this.player, Phaser.Physics.ARCADE);
