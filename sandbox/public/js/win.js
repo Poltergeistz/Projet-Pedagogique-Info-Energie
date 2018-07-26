0068f6db034b07081b5f2a9dd8a091b092e79958
david <david.journo.31@gmail.com>
2018-07-26 16:43:24 +0200

    End screen state, return to main menu

--- sandbox/public/js/win.js
+++ sandbox/public/js/win.js
@@ -7,12 +7,6 @@ var winState = {
         
         « Pour ce qui est de l’avenir, il ne s’agit pas de le prévoir mais de le rendre possible. »
          - Antoine de Saint-Exupéry`, { font: '15px Arial', fill: '#00FF00'});
-
-        var startLabel = game.add.text(80, game.world.height-80, 'press the "W" key to restart', { font: '25px Arial', fill: '#ffffff'})
-   
-        var wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-
-        wKey.onDown.addOnce(this.start, this);
     },
     restart: function(){
         game.state.start('menu');
