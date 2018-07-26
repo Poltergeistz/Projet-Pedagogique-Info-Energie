4b41a6df6ec55000f5f13fef705c3ea5ace62d45
david <david.journo.31@gmail.com>
2018-07-26 23:22:13 +0200

    Array rework

--- sandbox/public/levels/sdb_load.js
+++ sandbox/public/levels/sdb_load.js
@@ -2,27 +2,35 @@ var sdbLoadState = {
   preload: function() {
     // Array of Text
     var text = [
-      "Éteignez la lumière en quittant la pièce",
-      "Baisser le chauffage de 20 ° C à 19 ° C \n = - 7% de consommation d’énergie",
-      "Dégivrer son réfrigérateur \n = évite une surconsommation d’environ 30 %",
-      "Laver son linge à 30 °C  \n = 2 fois moins d’énergie qu’un lavage à 60 °C",
-      "Couvrir les casseroles pendant la cuisson \n = 4 fois moins d’électricité ou de gaz consommés",
-      "Si chaque ménage s’équipe en LED, on économise \n l’énergie d’une ville de 2 600 000 habitants",
-      "Laissez entrer la lumière dans les pièces",
-      "Utilisez un programmateur pour \n ne chauffer que lorsque vous êtes là",
-      "Éteignez la lumière en quittant la pièce",
-      "Fermez les volets et les rideaux la nuit \n pour garder la chaleur",
-      "Éteignez votre ordinateur \n et débranchez vos chargeurs après usage",
-      "Installez un mousseur sur les robinets \n pour économiser l’eau",
-      "Placez vos appareils de froid \n loin des sources de chaleur",
-      "Réglez votre chauffe-eau entre 55° et 60° C",
-      "Remplissez bien vos machines \n (lave-linge et lave-vaisselle)",
-      "1 bain = 5 douches !",
-      "Aérez les pièces 5 à 10 mn par jour même en hiver",
-      "Débranchez l’ordinateur \n (même éteint, il consomme de l'électricité)",
-      "Débranchez les chargeurs après utilisation",
-      "Évitez l'usage de radiateurs d'appoint électriques ou gaz",
-      "Éteignez simultanément \n les appareils électriques, à l’aide d’une multiprise"
+      `Éteignez la lumière en quittant la pièce`,
+      `Baisser le chauffage de 20 ° C à 19 ° C 
+      = - 7% de consommation d’énergie`,
+      `Dégivrer son réfrigérateur 
+      = évite une surconsommation d’environ 30 %`,
+      `Laver son linge à 30 °C 
+      = 2 fois moins d’énergie qu’un lavage à 60 °C`,
+      `Couvrir les casseroles pendant la cuisson 
+      = 4 fois moins d’électricité ou de gaz consommés`,
+      `Si chaque ménage s’équipe en LED, 
+      on économise l’énergie d’une ville de 2 600 000 habitants`,
+      `Laissez entrer la lumière dans les pièces`,
+      `Utilisez un programmateur 
+      pour ne chauffer que lorsque vous êtes là`,
+      `Éteignez la lumière en quittant la pièce`,
+      `Fermez les volets et les rideaux 
+      pendant la nuit pour garder la chaleur`,
+      `Éteignez votre ordinateur et débranchez vos chargeurs après usage`,
+      `Installez un mousseur sur les robinets pour économiser l’eau`,
+      `Placez vos appareils de froid loin des sources de chaleur`,
+      `Réglez votre chauffe-eau entre 55° et 60° C`,
+      `Remplissez bien vos machines (lave-linge et lave-vaisselle)`,
+      `1 bain = 5 douches !`,
+      `Aérez les pièces 5 à 10 mn par jour même en hiver`,
+      `Débranchez l’ordinateur 
+      (même éteint, il consomme de l'électricité)`,
+      `Débranchez les chargeurs après utilisation`,
+      `Évitez l'usage de radiateurs d'appoint électriques ou gaz`,
+      `Éteignez simultanément les appareils électriques, à l’aide d’une multiprise`
     ];
     // Random phrase generated from text Array
     var randomPhrase = text[Math.floor(Math.random() * text.length)];
