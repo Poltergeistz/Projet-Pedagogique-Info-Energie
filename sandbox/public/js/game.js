ea6908fb1d50d52bf2ba0c7c5129adf760164664
xLeDocteurx <lenoirc.fr@gmail.com>
2018-07-26 22:15:49 +0200

    Many many sprites added. Almost done placing back elements in the chambre level. I need to fix the size of the window and to make the bed. Then we will have to iterate inside it. I forgot the door. Also

--- sandbox/public/js/game.js
+++ sandbox/public/js/game.js
@@ -6,11 +6,12 @@ game.state.add('menu', menuState);
 // game.state.add('play', playState);
 
 game.state.add('intro', introState);
+game.state.add('chambre_load', chambreLoadState);
+game.state.add('chambre', chambreState);
+game.state.add('salon_load', salonState);
 game.state.add('salon', salonState);
 game.state.add('sdb_load', sdbLoadState);
 game.state.add('sdb', sdbState);
-game.state.add('chambre_load', chambreLoadState);
-game.state.add('chambre', chambreState);
 
 game.state.add('win', winState);
 
--- sandbox/public/levels/chambre.js
+++ sandbox/public/levels/chambre.js
@@ -1,24 +1,50 @@
 var chambre_time = 30;
 var timer, energyBar;
+
+var score = 0,
+//   seat,
+  desk,
+  bed,
+  ordi,
+  lamp,
+  curtains
+//   windowdown,
+//   player,
+//   platforms,
+//   cursors,
+//   scoreText,
+//   itemacct,
+//   spaceKey
+  ;
+
 var chambreState = {
 
     create: function () {
-
         // Background
-        game.add.sprite(0, 0, "sky");
-
+        game.add.sprite(0, 0, "backg");
+    
         // House
+        //  The platforms group contains the ground and the 2 ledges we can jump on
+        platforms = game.add.group();
+        //  We will enable physics for any object that is created in this group
+        platforms.enableBody = true;
+    
+        // Here we create the ground.
+        var ground = platforms.create(0, game.world.height - 87, "ground");
+    
+        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+        ground.scale.setTo(1, 6);
+    
+        //  This stops it from falling away when you jump on it
+        ground.body.immovable = true;
 
-        // Salle de Bain
-        game.add.sprite(50, 300, "etagere1");
-        game.add.sprite(650, 300, "etagere2");
-        game.add.sprite(100, 400, "meuble");
-        game.add.sprite(200, 400, "douche");
-        game.add.sprite(300, 400, "serviette");
-        game.add.sprite(400 - 64, 400, "double_lavabo");
-        game.add.sprite(500, 350, "peignoire");
-        game.add.sprite(600, 450, "toilettes");
-        game.add.sprite(700, 450, "pq");
+        // Chambre
+        game.add.sprite(100, 450, "bureau");
+        game.add.sprite(110, 405, "ordi");
+        game.add.sprite(165, 419, "lampe");
+        game.add.sprite(150, 320, "clock");
+        game.add.sprite(480, 417, "lit");
+        game.add.sprite(300, 300, "ch_fenetre");
 
         // The score
         timer = game.time.create();
