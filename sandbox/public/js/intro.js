98be1ad8baafdc27dda3b036dba432e0134a8d9e
david <david.journo.31@gmail.com>
2018-07-26 15:57:31 +0200

    Pitch Intro

--- sandbox/public/js/intro.js
+++ sandbox/public/js/intro.js
@@ -11,13 +11,11 @@ var introState = {
 
     var nameLabel = game.add.text(
       80,
-      80,
-      `Le pitch sur le professeur energeox
-      
-      Peut etre un texte défilant à la star wars ?
-      Ou
-      Unbackground defilant`,
-      { font: "30px Arial", fill: "#ffffff", align: "center" }
+      250,
+      `Le Professeur Energeox, fou de puissance, a rétrécie notre hero Ecokid. 
+      Votre but, est d'utiliser les gestes eco-responsables afin de priver 
+      Energeox de son pouvoir et, ainsi, retrouver votre taille normale.`,
+      { font: "20px Arial", fill: "#ffffff", align: "center" }
     );
 
     setTimeout(function() {
