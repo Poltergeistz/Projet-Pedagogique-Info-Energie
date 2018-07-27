986963c0ddcf06a9d4c5fe7a11fc250596aa3f91
david <david.journo.31@gmail.com>
2018-07-27 09:43:15 +0200

    spacekey variable

--- sandbox/public/levels/chambre.js
+++ sandbox/public/levels/chambre.js
@@ -2,68 +2,69 @@ var chambre_time = 30;
 var timer, energyBar;
 
 var score = 0,
-//   seat,
+  //   seat,
   bureau,
   ordi,
   lampe,
   lit,
   fenetre
-//   windowdown,
-//   player,
-//   platforms,
-//   cursors,
-//   scoreText,
-//   itemacct,
-//   spaceKey
+  //   windowdown,
+  //   player,
+  //   platforms,
+  //   cursors,
+  //   scoreText,
+  //   itemacct,
+  //   spaceKey
   ;
 
-  var ordiEteint, lampe2Eteinte, fenetreEteinte = false;
+var ordiEteint, lampe2Eteinte, fenetreEteinte = false;
 
 var chambreState = {
 
-    create: function () {
-        // Background
-        game.add.sprite(0, 0, "backg");
-    
-        // House
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = game.add.group();
-        //  We will enable physics for any object that is created in this group
-        platforms.enableBody = true;
-    
-        // Here we create the ground.
-        var ground = platforms.create(0, game.world.height - 87, "ground");
-    
-        //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-        ground.scale.setTo(1, 6);
-    
-        //  This stops it from falling away when you jump on it
-        ground.body.immovable = true;
+  create: function () {
+    // Background
+    game.add.sprite(0, 0, "backg");
 
-        // Chambre
-        game.add.sprite(100, 450, "bureau");
-        bureau = platforms.create(100, 450, "bureau");
-        bureau.body.immovable = true;
-        ordi = game.add.sprite(110, 405, "ordi");
-        game.physics.arcade.enable(ordi);
-        ordi.body.immovable = true;
-        spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-        lampe = game.add.sprite(165, 419, "lampe");
-        game.physics.arcade.enable(lampe);
-        lampe.body.immovable = true;
-        game.add.sprite(150, 320, "clock");
-        lit = game.add.sprite(480, 417, "lit");
-        lit.scale.setTo(0.75, 0.75);
+    // House
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
 
-        fenetre = game.add.sprite(300, 300, "ch_fenetre");
-        game.physics.arcade.enable(fenetre);
-        fenetre.body.immovable = true;
-        
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 87, "ground");
 
-        // Capture input from user
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(1, 6);
+
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    // Chambre
+    game.add.sprite(100, 450, "bureau");
+    bureau = platforms.create(100, 450, "bureau");
+    bureau.body.immovable = true;
+    ordi = game.add.sprite(110, 405, "ordi");
+    game.physics.arcade.enable(ordi);
+    ordi.body.immovable = true;
+    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    lampe = game.add.sprite(165, 419, "lampe");
+    game.physics.arcade.enable(lampe);
+    lampe.body.immovable = true;
+    game.add.sprite(150, 320, "clock");
+    lit = game.add.sprite(480, 417, "lit");
+    lit.scale.setTo(0.75, 0.75);
+
+    fenetre = game.add.sprite(300, 300, "ch_fenetre");
+    game.physics.arcade.enable(fenetre);
+    fenetre.body.immovable = true;
+
+    spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+
+    // Capture input from user
     cursors = game.input.keyboard.createCursorKeys();
 
-        // Create the player inside the lvl
+    // Create the player inside the lvl
     player = game.add.sprite(32, game.world.height - 250, "ecokid");
     game.physics.arcade.enable(player);
 
@@ -81,27 +82,28 @@ var chambreState = {
       true
     );
 
-        // The score
-        timer = game.time.create();
-        timerEvent = timer.add(Phaser.Timer.SECOND * chambre_time, this.gameOver, this);
-        timer.start();
-        energyBar = new HealthBar(this.game, {
-          width: 150,
-          height: 10,
-          x: 560,
-          y: 15,
-          bg: { color: "#651828" },
-          bar: { color: "#FEFF03" },
-          animationDuration: 200,
-          flipped: false
-        });
-        energyBar.setPercent(100);
+
+    // The score
+    timer = game.time.create();
+    timerEvent = timer.add(Phaser.Timer.SECOND * chambre_time, this.gameOver, this);
+    timer.start();
+    energyBar = new HealthBar(this.game, {
+      width: 150,
+      height: 10,
+      x: 560,
+      y: 15,
+      bg: { color: "#651828" },
+      bar: { color: "#FEFF03" },
+      animationDuration: 200,
+      flipped: false
+    });
+    energyBar.setPercent(100);
 
   },
-  update: function(){
-      energyBar.setPercent((timer.duration * 100) / (chambre_time * 1000));
-      game.physics.arcade.collide(player, platforms);
-      // When player stop moving
+  update: function () {
+    energyBar.setPercent((timer.duration * 100) / (chambre_time * 1000));
+    game.physics.arcade.collide(player, platforms);
+    // When player stop moving
     player.body.velocity.x = 0;
 
     // Controls
@@ -130,7 +132,7 @@ var chambreState = {
 
 
 
-  stopOrdi: function(){
+  stopOrdi: function () {
     if (spaceKey.isDown) {
       ordi.loadTexture('ordi_active');
       ordiEteint = true;
@@ -139,7 +141,7 @@ var chambreState = {
   },
 
 
-  stopLamp: function(){
+  stopLamp: function () {
     if (spaceKey.isDown) {
       lampe.loadTexture('lampe_active');
       lampe2Eteinte = true;
@@ -148,7 +150,7 @@ var chambreState = {
   },
 
 
-  stopWindow: function(){
+  stopWindow: function () {
     if (spaceKey.isDown) {
       fenetre.loadTexture('fenetre');
       fenetreEteinte = true;
